12bc859cc728b96286b991ab42caa0eb8f90ce19
FIGUREX <kennethggm12@gmail.com>
2022-02-16 21:24:30 -0600

    agregue que se pueda usar una API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { ProductItem } from '../components/ProductItem';
+import React, { useEffect, useState } from 'react';
+import { ProductItem } from '@components/ProductItem';
+import { useGetProducts } from '@hooks/useGetProducts.jsx';
 import '@styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products';
+
 function ProductList() {
+	const products = useGetProducts(API);
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map((product) => (
+					<ProductItem product={product} key={product.id} />
+				))}
 			</div>
 		</section>
 	);
